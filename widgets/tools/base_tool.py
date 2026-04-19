ba55edb87a77fc2982ba18c3316e17046b5415e0
Alex <alexanderchang140@gmail.com>
2026-04-19 00:39:37 -0700

    refactor: add typing for BaseTool

--- widgets/tools/base_tool.py
+++ widgets/tools/base_tool.py
@@ -1,4 +1,8 @@
+from PySide6.QtGui import QMouseEvent
+
+from widgets.canvas import Canvas
+
 class BaseTool:
-    def on_mouse_press(self, canvas, event): pass
-    def on_mouse_move(self, canvas, event): pass
-    def on_mouse_release(self, canvas, event): pass
+    def on_mouse_press(self, canvas: Canvas, event:QMouseEvent): pass
+    def on_mouse_move(self, canva: Canvas, event: QMouseEvent): pass
+    def on_mouse_release(self, canvas: Canvas, event: QMouseEvent): pass
