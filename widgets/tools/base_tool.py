ef246d0bf859f1d8035a622290940ef76db2ec3a
Alex <alexanderchang140@gmail.com>
2026-04-19 01:22:43 -0700

    fix: fix import circular import error for BaseTool

--- widgets/tools/base_tool.py
+++ widgets/tools/base_tool.py
@@ -1,8 +1,11 @@
+from typing import TYPE_CHECKING
+
 from PySide6.QtGui import QMouseEvent
 
-from widgets.canvas import Canvas
+if TYPE_CHECKING:
+    from widgets.canvas import Canvas
 
 class BaseTool:
-    def on_mouse_press(self, canvas: Canvas, event:QMouseEvent): pass
-    def on_mouse_move(self, canva: Canvas, event: QMouseEvent): pass
-    def on_mouse_release(self, canvas: Canvas, event: QMouseEvent): pass
+    def on_mouse_press(self, canvas: "Canvas", event:QMouseEvent): pass
+    def on_mouse_move(self, canva: "Canvas", event: QMouseEvent): pass
+    def on_mouse_release(self, canvas: "Canvas", event: QMouseEvent): pass
