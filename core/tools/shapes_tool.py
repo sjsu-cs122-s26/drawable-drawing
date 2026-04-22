1e27e39d5f06455b3384aa6feb09fb8009292df8
Joshua Lai <joshua.lai@sjsu.edu>
2026-04-22 16:36:56 -0700

    Added preview for users when creating shapes

--- core/tools/shapes_tool.py
+++ core/tools/shapes_tool.py
@@ -1,14 +1,15 @@
 from typing import override
 
-from PySide6.QtGui import QPainter, QPen, QBrush
+from PySide6.QtGui import QPainter, QPen, QBrush, QPolygon
 from PySide6.QtCore import Qt, QRect, QPoint
 
 from core.tools.base_tool import BaseTool
 
 class ShapesTool(BaseTool):
     def __init__(self):
-        self.shape = "rectangle"  # default
+        self.shape = "rectangle"
         self.start_point = QPoint()
+        self.current_point = QPoint()
 
     def setShape(self, shape):
         self.shape = shape.lower()
@@ -16,10 +17,12 @@ class ShapesTool(BaseTool):
     @override
     def on_mouse_press(self, canvas, event):
         self.start_point = event.position().toPoint()
+        self.current_point = self.start_point
 
     @override
     def on_mouse_move(self, canvas, event):
-        canvas.update()  # just refresh preview
+        self.current_point = event.position().toPoint()
+        canvas.update()
 
     @override
     def on_mouse_release(self, canvas, event):
@@ -37,7 +40,6 @@ class ShapesTool(BaseTool):
         elif self.shape == "line":
             painter.drawLine(self.start_point, end_point)
         elif self.shape == "triangle":
-            from PySide6.QtGui import QPolygon
             triangle = QPolygon([
                 self.start_point,
                 QPoint(end_point.x(), self.start_point.y()),
@@ -47,3 +49,23 @@ class ShapesTool(BaseTool):
 
         painter.end()
         canvas.update()
+
+    def draw_preview(self, canvas):
+        painter = QPainter(canvas)
+        painter.setPen(QPen(canvas.color, 2, Qt.PenStyle.DashLine))
+        painter.setBrush(QBrush(canvas.color))
+        rect = QRect(self.start_point, self.current_point).normalized()
+        if self.shape == "rectangle":
+            painter.drawRect(rect)
+        elif self.shape == "ellipse":
+            painter.drawEllipse(rect)
+        elif self.shape == "line":
+            painter.drawLine(self.start_point, self.current_point)
+        elif self.shape == "triangle":
+            triangle = QPolygon([
+                self.start_point,
+                QPoint(self.current_point.x(), self.start_point.y()),
+                QPoint((self.start_point.x() + self.current_point.x()) // 2, self.current_point.y())
+            ])
+            painter.drawPolygon(triangle)
+        painter.end()
