1320d21c4230e03e933a8bf9ab3dce939126e80d
davidNguyen4321 <daviddmnguyen@gmail.com>
2026-04-24 17:19:47 -0700

    Refactor: removed a redundant line of code in the eraser tool.

--- core/tools/eraser_tool.py
+++ core/tools/eraser_tool.py
@@ -1,5 +1,5 @@
 from typing import override
-from PySide6.QtGui import QPainter, QBrush, QPen
+from PySide6.QtGui import QPainter, QPen
 from PySide6.QtCore import Qt
 from core.tools.base_tool import BaseTool
 
@@ -11,7 +11,6 @@ class EraserTool(BaseTool):
         painter = QPainter(canvas.currentLayer.image)
         painter.setCompositionMode(QPainter.CompositionMode.CompositionMode_Clear)
         painter.setPen(QPen(Qt.GlobalColor.transparent, self.eraser_width, Qt.PenStyle.SolidLine, Qt.PenCapStyle.RoundCap, Qt.PenJoinStyle.RoundJoin))
-        painter.setBrush(QBrush(Qt.GlobalColor.transparent))
         painter.drawLine(canvas.last_point, point)
         canvas.last_point = point
         painter.end()
